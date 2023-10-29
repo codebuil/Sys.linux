28459d30859341258888a83824926156f0d3c600
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-10-29 13:58:00 +0000

    Add files via upload

--- hard.sh
+++ hard.sh
@@ -1,12 +1,17 @@
 printf "\ec\e[43;37m"
-unzip bin.zip 
+unzip bin.zip -a
 dd if=/dev/zero of="hard.img" bs=1M count=10
 sudo mkfs.vfat -n 'HARD' -S 512  "hard.img"
 sudo mkdir /mnt/new
 sudo chmod 777 /mnt/new
 mount -t vfat -o loop "hard.img" /mnt/new
 mkdir /mnt/new/syslinux/
+mkdir /mnt/new/syslinux/boot
 mkdir /mnt/new/boot/
+mkdir /mnt/new/boot/syslinux/
+cp ./syslinux/*.* /mnt/new/syslinux/
+cp ./syslinux/*.* /mnt/new/syslinux/boot
+cp ./syslinux/*.* /mnt/new/syslinux/boot
 umount  /mnt/new
 syslinux hard.img
 
