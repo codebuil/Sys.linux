af35aa8def318060aa5136db494d6cab70199342
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-10-31 16:25:14 +0000

    Add files via upload

--- hard.sh
+++ hard.sh
@@ -1,6 +1,6 @@
 printf "\ec\e[43;37m"
-unzip bin.zip -a
-dd if=/dev/zero of="hard.img" bs=1M count=10
+unzip bin.zip 
+dd if=/dev/zero of="hard.img" bs=1M count=4
 sudo mkfs.vfat -n 'HARD' -S 512  "hard.img"
 sudo mkdir /mnt/new
 sudo chmod 777 /mnt/new
